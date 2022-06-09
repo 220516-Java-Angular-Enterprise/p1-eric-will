34976092c3d53fc8482d0694cb675f95f9067bef
William Marquardt <william940@revature.net>
2022-06-09 16:40:02 -0500

    more work on reimbServlets

--- src/main/java/com/revature/ers/services/ReimbursementsServices.java
+++ src/main/java/com/revature/ers/services/ReimbursementsServices.java
@@ -1,4 +1,38 @@
 package com.revature.ers.services;
 
+import com.revature.ers.daos.ReimbursementsDAO;
+import com.revature.ers.daos.UsersDAO;
+import com.revature.ers.dtos.requests.NewReimbRequest;
+import com.revature.ers.dtos.requests.NewUserRequest;
+import com.revature.ers.models.Reimbursements;
+import com.revature.ers.models.Users;
+import com.revature.ers.util.custom_exceptions.InvalidRequestException;
+import com.revature.ers.util.custom_exceptions.ResourceConflictException;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.util.List;
+import java.util.UUID;
+
 public class ReimbursementsServices {
+    private final ReimbursementsDAO reimbDAO;
+    public ReimbursementsServices(ReimbursementsDAO reimbDAO){
+        this.reimbDAO = reimbDAO;
+    }
+
+    public Reimbursements register(NewReimbRequest request){
+        Reimbursements reimb = request.extractReimb();
+        if (isValidReimbType(request.getType_id())){
+                    reimb.setReimb_id(UUID.randomUUID().toString());
+                    reimbDAO.save(reimb);
+        } else throw new InvalidRequestException("Invalid reimbursement type.");
+        return reimb;
+    }
+
+    private boolean isValidReimbType(String reimbType){
+        List<String> reimbTypes=reimbDAO.getAllReimbTypes();
+        reimbTypes.forEach();
+    }
+
+
+
 }
