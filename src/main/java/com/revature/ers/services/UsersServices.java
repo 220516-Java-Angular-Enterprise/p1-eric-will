6e0003c02aa28254e1d5dc386c7a5be54941cbdc
Phutondog <ericmphu@gmail.com>
2022-06-13 17:32:58 -0400

    stuff

--- src/main/java/com/revature/ers/services/UsersServices.java
+++ src/main/java/com/revature/ers/services/UsersServices.java
@@ -29,8 +29,6 @@ public class UsersServices {
             if(isValidUsername(username)){
                 if(isValidPassword(user.getPassword())){
                     user.setUser_id(UUID.randomUUID().toString());
-
-
                     usersDAO.save(user);
                 } else throw new InvalidRequestException("Invalid password. Minimum eight characters, at least one letter, one number and one special character.");
             } else throw new InvalidRequestException("Invalid username. Username needs to be 8-20 characters long.");
@@ -48,11 +46,11 @@ public class UsersServices {
         return !usernames.contains(username);
     }
 
-    public boolean isValidUsername(String username) {
+    private boolean isValidUsername(String username) {
         return username.matches("^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$");
     }
 
-    public boolean isValidPassword(String password) {
+    private boolean isValidPassword(String password) {
         return password.matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
     }
 
--- src/test/java/com/revature/ers/services/UsersServicesTest.java
+++ src/test/java/com/revature/ers/services/UsersServicesTest.java
@@ -1,19 +1,36 @@
 package com.revature.ers.services;
 
 import com.revature.ers.daos.UsersDAO;
+import com.revature.ers.models.Users;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UsersServicesTest {
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Mock
     private UsersDAO mockDAO;
 
     @InjectMocks
     private UsersServices usersServices;
+
+    @Rule
+    public MockitoRule initRule = MockitoJUnit.rule();
+
+    @Spy
+    Users fakeUser= new Users();
 
     @Test
     void register() {
